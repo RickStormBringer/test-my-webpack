51b2673e3209fdf152ead74ddaa02d0bc7ba952f
yangyang.hu <yangyang.hu@matrixport.com>
2019-12-16 20:12:49 +0800

    fix: 🐛 fix wrong scripts

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: 胡阳阳
  * @Date: 2019-12-12 07:55:36
- * @LastEditTime: 2019-12-12 08:20:36
+ * @LastEditTime: 2019-12-16 20:12:19
  * @LastEditors: 胡阳阳
  */
 const assert = require('assert');
@@ -11,7 +11,7 @@ describe('test webpack base test', () => {
   const baseConfig = require('../../lib/webpack.base');
   console.log(baseConfig);
   it('entry', () => {
-    assert.equal(baseConfig.entry.index, './src/index/index.js');
-    assert.equal(baseConfig.entry.search, './src/search/index.js');
+    assert.equal(baseConfig.entry.index.indexOf('./src/index/index.js') > -1, true);
+    assert.equal(baseConfig.entry.search.indexOf('./src/search/index.js') > -1, true);
   });
 });
